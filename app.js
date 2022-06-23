092f5d6259758f4f4dc8a4c47a07585430805aaf
Victor Karvatsky <victor.karvatsky@gmail.com>
2022-06-23 18:40:06 +0300

    console router

--- app.js
+++ app.js
@@ -44,23 +44,8 @@ app.use(sessionParser);
 
 app.use('/auth', authRouter);
 
-// catch 404 and forward to error handler
-app.use((req, res, next) => {
-  next(createError(404));
-});
-
-// error handler§
-app.use((err, req, res) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
 app.get('/*', (req, res) => {
+  console.log('router get * ---->');
   res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
 });
 
